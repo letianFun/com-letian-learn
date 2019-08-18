36997cfe7b8abc965cb91b210a46d00e4c089328
lh <li_hao80@163.com>
2019-08-18 14:52:57 +0800

    工具包和架构

---
+++ com-letian-learn-se/src/main/java/com/letian/learn/ComLetianLearnSeApplication.java
@@ -0,0 +1,13 @@
+package com.letian.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComLetianLearnSeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComLetianLearnSeApplication.class, args);
+    }
+
+}
---
+++ com-letian-learn-thirdparty/src/main/java/com/letian/learn/thirdparty/ComLetianLearnThirdpartyApplication.java
@@ -0,0 +1,13 @@
+package com.letian.learn.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComLetianLearnThirdpartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComLetianLearnThirdpartyApplication.class, args);
+    }
+
+}
